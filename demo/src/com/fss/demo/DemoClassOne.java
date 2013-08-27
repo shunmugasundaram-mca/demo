42533ab2bf1cbe1ef517b2e86516bdd484dc83cc
karthikeyans <karthikeyans@fss.co.in>
2013-08-27 16:47:01 +0530

    first commit

---
+++ demo/src/com/fss/demo/DemoClassOne.java
@@ -0,0 +1,13 @@
+package com.fss.demo;
+
+public class DemoClassOne {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Demo One........");
+	}
+
+}
